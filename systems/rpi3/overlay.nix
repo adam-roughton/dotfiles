52f28ff84c78c79b1be7b3a549f38da59ca6adfa
Adam Roughton <adam.roughton@gmail.com>
2021-12-30 08:09:56 +1300

    Add raspberrypifw override (WIP)

--- systems/rpi3/overlay.nix
+++ systems/rpi3/overlay.nix
@@ -1,4 +1,15 @@
 self: super:
 {
   imports = [ ../../packages ];
+
+  # Use the most update to date firmware
+  raspberrypifw = super.raspberrypifw.overrideAttrs (old: rec {
+    version = "1.20211118";
+    src = super.fetchFromGitHub {
+      owner = "raspberrypi";
+      repo = "firmware";
+      rev = version;
+      sha256 = "1nndhjv4il42yw3pq8ni3r4nlp1m0r229fadrf4f9v51mgcg11i1";
+    };
+  });
 }
